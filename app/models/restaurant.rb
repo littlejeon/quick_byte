6867365503b29a6fbedf85f7d1b0fcc55904caf7
fullstack212 <keren.lerner@flatironschool.com>
2016-01-29 15:04:47 -0500

    add logic to restaurant show page

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,21 +3,44 @@ class Restaurant < ActiveRecord::Base
   has_many :users, through: :reviews
 
   def average_wait_time
-    wait_times = self.reviews.map { |review| review.wait_time }
-    sum_of_wait_times = wait_times.inject {|result, num| result + num }
-    sum_of_wait_times / wait_times.count
+    wait_times = self.reviews.map do |review| 
+      if review.wait_time 
+        review.wait_time 
+      end
+    end.compact
+      #binding.pry
+
+    unless wait_times.empty?    
+      sum_of_wait_times = wait_times.inject {|result, num| result + num }
+      sum_of_wait_times / wait_times.count
+    end
+
   end
 
   def average_rating
-    all_ratings = self.reviews.map { |review| review.rating }
-    sum_of_ratings = all_ratings.inject {|result, num| result + num }
-    sum_of_ratings / all_ratings.count
+    all_ratings = self.reviews.map do |review| 
+    #binding.pry
+      if review.rating 
+        review.rating 
+      end
+    end.compact
+
+      unless all_ratings.empty?
+        sum_of_ratings = all_ratings.inject {|result, num| result + num }
+        sum_of_ratings / all_ratings.count
+        end
   end
 
   def average_price
-    all_prices = self.reviews.map { |review| review.rating }
+    all_prices = self.reviews.map do |review| 
+      if review.price 
+        review.price 
+      end
+    end.compact
+      unless all_prices.empty?
     sum_of_prices = all_prices.inject {|result, num| result + num }
     sum_of_prices / all_prices.count
+  end
   end
 
   def comments
@@ -25,3 +48,4 @@ class Restaurant < ActiveRecord::Base
   end
 
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 20160128202504) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
     t.string   "name"
-    t.string   "image_url"
   end
 
   create_table "reviews", force: :cascade do |t|
