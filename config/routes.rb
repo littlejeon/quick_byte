fd6ee131afaab36c1603eacf42d8d784ee85254a
littlejeon <jeon.lisa@gmail.com>
2016-01-29 09:44:19 -0500

    Finished sessions and added styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,9 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-  get 'sessions/create'
 
-  get 'sessions/new'
-
-
-
-  get '/login', :to => 'sessions#login'
+  get '/login', :to => 'sessions#new'
   delete '/logout', :to => 'sessions#destroy'
   get '/signup', :to => 'users#new'
-
-
-  get 'sessions/destroy'
-
-
-  get 'sessions/show'
 
   resources :users
   resources :sessions
