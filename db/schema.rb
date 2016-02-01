9280da1d1772a618c7e88807b99be4a344859fd7
littlejeon <jeon.lisa@gmail.com>
2016-02-01 10:02:32 -0500

    Pushes up changes from merge

--- db/schema.rb
+++ db/schema.rb
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 20160201013313) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
     t.string   "name"
-    t.string   "image_url"
     t.integer  "type_id"
   end
 
@@ -51,6 +50,15 @@ ActiveRecord::Schema.define(version: 20160201013313) do
   add_index "reviews", ["restaurant_id"], name: "index_reviews_on_restaurant_id", using: :btree
   add_index "reviews", ["user_id"], name: "index_reviews_on_user_id", using: :btree
 
+  create_table "types", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "types", ["user_id"], name: "index_types_on_user_id", using: :btree
+
   create_table "user_organizations", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "organization_id"
@@ -62,15 +70,6 @@ ActiveRecord::Schema.define(version: 20160201013313) do
   add_index "user_organizations", ["organization_id"], name: "index_user_organizations_on_organization_id", using: :btree
   add_index "user_organizations", ["user_id"], name: "index_user_organizations_on_user_id", using: :btree
 
-  create_table "types", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "types", ["user_id"], name: "index_types_on_user_id", using: :btree
-
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
@@ -79,13 +78,10 @@ ActiveRecord::Schema.define(version: 20160201013313) do
     t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "restaurants", "types"
   add_foreign_key "reviews", "restaurants"
   add_foreign_key "reviews", "users"
+  add_foreign_key "types", "users"
   add_foreign_key "user_organizations", "organizations"
   add_foreign_key "user_organizations", "users"
-  add_index "users", ["organization_id"], name: "index_users_on_organization_id", using: :btree
-
-  add_foreign_key "restaurants", "types"
-  add_foreign_key "types", "users"
-  add_foreign_key "users", "organizations"
 end
