d29219f543602cb0e791dc1878752f03ac6ff9db
antoinfive <antoincampbell@live.com>
2016-02-01 14:17:45 -0500

    migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160201013313) do
+ActiveRecord::Schema.define(version: 20160201154118) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20160201013313) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
     t.string   "name"
+    t.string   "image_url"
     t.integer  "type_id"
   end
 
@@ -62,9 +63,9 @@ ActiveRecord::Schema.define(version: 20160201013313) do
   create_table "user_organizations", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "organization_id"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.boolean  "admin"
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.boolean  "admin",           default: false
   end
 
   add_index "user_organizations", ["organization_id"], name: "index_user_organizations_on_organization_id", using: :btree
@@ -76,11 +77,10 @@ ActiveRecord::Schema.define(version: 20160201013313) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "organization_id"
   end
 
-
   add_index "users", ["organization_id"], name: "index_users_on_organization_id", using: :btree
-
 
   add_foreign_key "restaurants", "types"
   add_foreign_key "reviews", "restaurants"
@@ -88,5 +88,5 @@ ActiveRecord::Schema.define(version: 20160201013313) do
   add_foreign_key "types", "users"
   add_foreign_key "user_organizations", "organizations"
   add_foreign_key "user_organizations", "users"
-
+  add_foreign_key "users", "organizations"
 end
