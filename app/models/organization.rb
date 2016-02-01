61b20944e76d9c01de564bd43b16f490dbbeccbc
littlejeon <jeon.lisa@gmail.com>
2016-02-01 18:14:19 -0500

    remove has_many :users line from org model b/c it duplicates has_many users through user_orgs. this is left over from before there was such a join table. next, need to remove org_id from users table

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,8 +1,6 @@
 class Organization < ActiveRecord::Base
   has_many :user_organizations
   has_many :users, through: :user_organizations
-
-  has_many :users
   has_many :plans, through: :users
 
   has_many :restaurants, through: :users
