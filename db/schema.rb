1d9bafba7be0ddc1486e0f1ac6664c9cedbae450
antoinfive <antoincampbell@live.com>
2016-02-02 18:22:51 -0500

    merge conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20160202171340) do
 
   # These are extensions that must be enabled in order to support this database
@@ -101,9 +100,11 @@ ActiveRecord::Schema.define(version: 20160202171340) do
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.integer  "organization_id"
+    t.boolean  "email_confirmed", default: false
+    t.string   "confirm_token"
   end
 
   add_index "users", ["organization_id"], name: "index_users_on_organization_id", using: :btree
