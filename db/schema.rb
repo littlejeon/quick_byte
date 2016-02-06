fa0f9a554e2db1a5367f94a69140598f311fbe84
antoinfive <antoincampbell@live.com>
2016-02-06 15:12:49 -0500

    wat

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160204192941) do
+ActiveRecord::Schema.define(version: 20160205155251) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,18 @@ ActiveRecord::Schema.define(version: 20160204192941) do
   end
 
   add_index "cuisines", ["user_id"], name: "index_cuisines_on_user_id", using: :btree
+
+  create_table "locations", force: :cascade do |t|
+    t.string   "address"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "street"
+    t.string   "city"
+    t.string   "state"
+    t.string   "country"
+  end
 
   create_table "organizations", force: :cascade do |t|
     t.string   "name"
@@ -105,12 +117,12 @@ ActiveRecord::Schema.define(version: 20160204192941) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.integer  "organization_id"
-    t.boolean  "email_confirmed",     default: false
-    t.string   "confirm_token"
     t.string   "avatar_file_name"
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.boolean  "email_confirmed",     default: false
+    t.string   "confirm_token"
     t.text     "bio"
     t.string   "position"
   end
