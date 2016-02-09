b48a58ff2f0591f1d5a570c81177865d98a85eca
littlejeon <jeon.lisa@gmail.com>
2016-02-09 01:22:55 -0500

    trys to fix paperclip

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -68,6 +68,7 @@ class UsersController < ApplicationController
 
   def update
     user = User.find(params[:id])
+    # binding.pry
     if user.update_attributes(user_params)
       redirect_to user, :notice => "Your settings have been updated!"
     else
